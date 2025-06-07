ec3c8eaab98fa75934b28c284eaa70899022527a
Federico Díaz <federicodiazbobillo@gmail.com>
2025-06-07 12:21:25 -0300

    .

--- app/__init__.py
+++ app/__init__.py
@@ -1,15 +1,24 @@
 from flask import Flask
 from flask_mysqldb import MySQL
 from app.config import Config
+import MySQLdb
 
 mysql = MySQL()
 
 def create_app():
-    # Asegura que Flask encuentre correctamente static/ y templates/
     app = Flask(__name__, static_folder='static', template_folder='templates')
     app.config.from_object(Config)
-
     mysql.init_app(app)
+
+    # Verificar conexión a la base de datos
+    try:
+        with app.app_context():
+            conn = mysql.connection
+            cursor = conn.cursor()
+            cursor.execute("SELECT 1")
+            app.jinja_env.globals['db_connected'] = True
+    except Exception:
+        app.jinja_env.globals['db_connected'] = False
 
     from app.controllers.auth import auth_bp
     from app.controllers.home import home_bp
