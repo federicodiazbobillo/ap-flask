1af01585b87216c7cb1732d172acb00cad16d5c9
Federico Díaz <federicodiazbobillo@gmail.com>
2025-06-07 10:29:59 -0300

    configuracion de la Base de datos

---
+++ app/__init__.py
@@ -0,0 +1,19 @@
+from flask import Flask
+from flask_mysqldb import MySQL
+from app.config import Config
+
+mysql = MySQL()
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(Config)
+
+    mysql.init_app(app)
+
+    from app.controllers.auth import auth_bp
+    from app.controllers.orders import orders_bp
+
+    app.register_blueprint(auth_bp)
+    app.register_blueprint(orders_bp)
+
+    return app
