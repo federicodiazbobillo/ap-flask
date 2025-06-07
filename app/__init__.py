3bf321b8983a407539ac567a0fde8dcb8db373fa
Federico Díaz <federicodiazbobillo@gmail.com>
2025-06-07 13:59:14 -0300

    .

--- app/__init__.py
+++ app/__init__.py
@@ -1,19 +1,16 @@
 import os
 from flask import Flask
 from app.config import Config
-from app.db import get_conn  # ACCESO ÚNICO
-from app.integrations.mercadolibre.verificar_token import verificar_meli
+from app.db import get_conn
 from app.utils.blueprint_loader import register_blueprints
 from app.extensions import mysql
-
-
 
 def create_app():
     app = Flask(__name__, static_folder='static', template_folder='templates')
     app.config.from_object(Config)
     mysql.init_app(app)
 
-    # Verificar conexión a la base de datos
+    # Validar conexión a la base de datos
     try:
         with app.app_context():
             conn = get_conn()
@@ -23,16 +20,9 @@ def create_app():
     except Exception:
         app.jinja_env.globals['db_connected'] = False
 
-    # Verificar token de Mercado Libre
-    try:
-        token_valido = verificar_meli()
-        app.jinja_env.globals['meli_token_valido'] = token_valido
-    except Exception:
-        app.jinja_env.globals['meli_token_valido'] = False
-
-    # Registro automático de blueprints
+    # Registrar blueprints automáticamente
     register_blueprints(app, 'app.controllers', os.path.join(app.root_path, 'controllers'))
     register_blueprints(app, 'app.integrations', os.path.join(app.root_path, 'integrations'))
 
-
     return app
+ 
