8d540e4108f9f55e9719d72488f3c46d44a629ab
Federico Díaz <federicodiazbobillo@gmail.com>
2025-06-08 16:15:49 -0300

    .

--- app/controllers/orders/routes.py
+++ app/controllers/orders/routes.py
@@ -1,5 +1,5 @@
 from flask import Blueprint
-from app.controllers.orders.orders_costos_controller import orders_costos_bp
-from app.controllers.orders.orders_logistica_controller import orders_logistica_bp
+#from app.controllers.orders.orders_costos_controller import orders_costos_bp
+#from app.controllers.orders.orders_logistica_controller import orders_logistica_bp
 
 orders_bp = Blueprint('orders_bp', __name__)
