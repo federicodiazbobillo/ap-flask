48589ca5ce9c483dd288a17cb5eac531eeeaa384
Federico Díaz <federicodiazbobillo@gmail.com>
2025-06-09 20:35:45 -0300

    .

--- app/test.py
+++ app/test.py
@@ -1,27 +1,25 @@
 import requests
 
-# Datos de entrada
-order_id = "2000011896543284"
+order_id = "2000011629033904"
 access_token = "APP_USR-1700552002175315-060919-b11c58926da1370f426fa87b59413bd9-2025384704"
 nota = "Esta es una nota de prueba desde el script Python."
 
-# URL del endpoint
 url = f"https://api.mercadolibre.com/orders/{order_id}/notes?access_token={access_token}"
 
-# Cuerpo de la nota
 payload = {
     "note": nota
 }
 
-# Encabezados
 headers = {
     "Content-Type": "application/json"
 }
 
-# Enviar la solicitud POST
 response = requests.post(url, json=payload, headers=headers)
 
-# Mostrar respuesta
 print(f"Status code: {response.status_code}")
-print("Respuesta:")
-print(response.json())
+
+try:
+    print(response.json())
+except Exception as e:
+    print("No se pudo parsear JSON. Respuesta cruda:")
+    print(response.text)
