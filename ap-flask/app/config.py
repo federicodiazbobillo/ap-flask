8472e2854e08bfcfd8479f6749e26189398f5dfd
Federico Diaz Bobillo <federiocdiazbobillo@gmail.com>
2025-06-18 12:30:22 -0300

    .

--- ap-flask/app/config.py
+++ ap-flask/app/config.py
@@ -4,9 +4,9 @@ from dotenv import load_dotenv
 load_dotenv()  # Carga las variables desde .env
 
 class Config:
-    SECRET_KEY = os.getenv('SECRET_KEY')
-    MYSQL_HOST = os.getenv('MYSQL_HOST')
-    MYSQL_USER = os.getenv('MYSQL_USER')
-    MYSQL_PASSWORD = os.getenv('MYSQL_PASSWORD')
-    MYSQL_DB = os.getenv('MYSQL_DB')
- 
+    SECRET_KEY = os.getenv('SECRET_KEY', 'clave-por-defecto')
+    MYSQL_HOST = os.getenv('MYSQL_HOST', 'localhost')
+    MYSQL_USER = os.getenv('MYSQL_USER', 'root')
+    MYSQL_PASSWORD = os.getenv('MYSQL_PASSWORD', '')
+    MYSQL_DB = os.getenv('MYSQL_DB', 'test')
+    MYSQL_PORT = int(os.getenv('MYSQL_PORT', 3306))
