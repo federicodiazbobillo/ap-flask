439f9640379acedcab29aa54225fe0317b353c75
Federico Diaz Bobillo <federiocdiazbobillo@gmail.com>
2025-06-21 17:00:38 -0300

    version 1.0

--- ap-flask/app/controllers/items/mercadolibre/catalogador/__init__.py
+++
@@ -1,1 +0,0 @@
-from .index_controller import items_meli_bp
--- ap-flask/app/controllers/items/mercadolibre/catalogador/index_controller.py
+++ ap-flask/app/controllers/items/mercadolibre/catalogador/index_controller.py
@@ -2,17 +2,17 @@ from flask import Blueprint, render_template, request
 from .listado import obtener_items
 from .catalogar import procesar_catalogacion
 
-items_meli_bp = Blueprint(
-    'items_meli_bp',
+catalogador_bp = Blueprint(
+    'catalogador_bp',
     __name__,
     template_folder='app/templates/items/mercadolibre'
 )
 
-@items_meli_bp.route('/items/mercadolibre')
+@catalogador_bp.route('/items/mercadolibre')
 def items():
     context = obtener_items(request)
     return render_template('items/mercadolibre/items.html', **context)
 
-@items_meli_bp.route('/catalogar', methods=['POST'])
+@catalogador_bp.route('/catalogar', methods=['POST'])
 def catalogar_items():
     return procesar_catalogacion(request)
