c2faf39d884b52bf2044158b53d9652e7293a767
Federico Diaz Bobillo <federiocdiazbobillo@gmail.com>
2025-06-24 10:12:58 -0300

    .

--- ap-flask/app/controllers/items/mercadolibre/promociones/index_controller.py
+++ ap-flask/app/controllers/items/mercadolibre/promociones/index_controller.py
@@ -1,12 +1,14 @@
+# promociones_controller.py
+import requests
 from flask import Blueprint, render_template
+from app.integrations.mercadolibre.services.token_service import verificar_meli
 
-meli_promociones_bp = Blueprint(
-    'meli_promociones_bp',
-    __name__,
-    url_prefix='/items/mercadolibre/promociones',
-    template_folder='app/templates/items/mercadolibre/promociones'
-)
+promociones_bp = Blueprint("promociones", __name__, url_prefix="/meli/promociones")
 
-@meli_promociones_bp.route('/')
-def index():
-    return render_template('items/mercadolibre/promociones/index.html')
+@promociones_bp.route("/")
+def listar_promociones():
+    access_token, user_id, error = verificar_meli()
+    url = f"https://api.mercadolibre.com/seller-promotions/users/2025384704?app_version=v2&access_token={access_token}"
+    resp = requests.get(url)
+    promociones = resp.json().get("promotions", [])
+    return render_template("items/mercadolibre/promociones/index.html", promociones=promociones)
