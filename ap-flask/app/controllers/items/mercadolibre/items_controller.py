4b208946475d5e90c02ce24650a6ea8fa176bae4
Federico Diaz Bobillo <federicodiazbobillo@gmail.com>
2025-08-07 12:21:16 -0300

    .

--- ap-flask/app/controllers/items/mercadolibre/items_controller.py
+++ ap-flask/app/controllers/items/mercadolibre/items_controller.py
@@ -11,15 +11,17 @@ items_mercadolibre_bp = Blueprint(
 @items_mercadolibre_bp.route('/sin_isbn')
 def items_sin_isbn():
     conn = get_conn()
-    with conn.cursor(dictionary=True) as cursor:
-        cursor.execute("""
-            SELECT isbn, seller_sku, items_meli.idml
-            FROM items_meli, order_items 
-            WHERE 
-              CONVERT(items_meli.idml USING utf8mb4) COLLATE utf8mb4_unicode_ci = 
-              CONVERT(order_items.item_id USING utf8mb4) COLLATE utf8mb4_unicode_ci
-              AND order_items.seller_sku IS NULL
-        """)
-        resultados = cursor.fetchall()
+    cursor = conn.cursor()
+    cursor.execute("""
+        SELECT isbn, seller_sku, items_meli.idml
+        FROM items_meli, order_items 
+        WHERE 
+          CONVERT(items_meli.idml USING utf8mb4) COLLATE utf8mb4_unicode_ci = 
+          CONVERT(order_items.item_id USING utf8mb4) COLLATE utf8mb4_unicode_ci
+          AND order_items.seller_sku IS NULL
+    """)
+    columnas = [col[0] for col in cursor.description]
+    resultados = [dict(zip(columnas, fila)) for fila in cursor.fetchall()]
+    cursor.close()
     conn.close()
     return render_template("items/mercadolibre/sin_isbn.html", items=resultados)
