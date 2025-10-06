729ba335cad4b015eb568758cdde6ea6755c6046
Federico Diaz Bobillo <federicodiazbobillo@gmail.com>
2025-10-06 08:40:42 -0300

    Limpiar .gitignore y estructura de proyecto

--- run.py
+++ run.py
@@ -3,4 +3,4 @@ from app import create_app
 app = create_app()
 
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0')
+    app.run(host="127.0.0.1", port=80, debug=False, use_reloader=False)
